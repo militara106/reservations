a59ac02526e2b077ef0f525c3510bc3ad4b34945
jon <jcrossguitar@gmail.com>
2019-09-14 12:37:39 -0700

    I added more js and cleaned up nomenclature. I added a forloop and the code for creating a new character

--- server.js
+++ server.js
@@ -9,29 +9,79 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+
+
+// Displays all characters
+app.get("/api/characters", function(req, res) {
+  return res.json(characters);
+});
+
 var characters = [
   {
     routeName: "yoda",
     name: "Yoda",
-    email: "Jedi Master",
-    phoneNumber: 900,
-    uniqueID: 4603
+    phone: "18009002424",
+    email: "jcrossguitar@gmail.com",
+    unique: 4603
   },
   {
-    routeName: "yoda",
-    name: "Yoda",
-    email: "Jedi Master",
-    phoneNumber: 900-439-0011,
-    uniqueID: 2103
+    routeName: "jc",
+    name: "jc",
+    phone: "Jedi Master",
+    email: 900-439-0011,
+    unique: 2103
   },
   {
-    routeName: "yoda",
-    name: "Yoda",
-    email: "Jedi Master",
-    phoneNumber: 900,
-    uniqueID: 5395
+    routeName: "tommy",
+    name: "tommy",
+    phone: 800-940-3636,
+    email: "jcrcossguitar@gmail.com",
+    unique: 5395
   }
 ];
+
+app.get("/api/characters/:character", function(req, res) {
+  var chosen = req.params.character;
+
+  console.log(chosen);
+
+for (var i = 0; i < characters.length; i++) {
+  if (chosen === characters[i].routeName) {
+    return res.json(characters[i]);
+  }
+}
+
+return res.json(false);
+});
+
+app.post("/api/characters", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newCharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newCharacter);
+
+  characters.push(newCharacter);
+
+  res.json(newCharacter);
+});
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
