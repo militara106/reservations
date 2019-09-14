6c2d2673c0ff1137c086cec7b7a3e33095c30ccc
Bill Hammond <willmhammond@gmail.com>
2019-09-14 12:04:56 -0700

    addedea set of variables for holding the reservation and waitlist

---
+++ tableData.js
@@ -0,0 +1,15 @@
+// ===============================================
+// empoty array for table data
+
+var tableArray = [
+    {
+    routeName: "",
+    customerName: "",
+    email: "",
+    phoneNumber: "",
+    customerId:""
+}
+];
+
+// export module 
+module.exports = tableArray;
---
+++ waitList.js
@@ -0,0 +1,15 @@
+// ===============================================
+// empoty array for table data
+
+var waitArray = [
+    {
+    routeName: "",
+    customerName: "",
+    email: "",
+    phoneNumber: "",
+    customerId:""
+}
+];
+
+// export module 
+module.exports = waitArray;
